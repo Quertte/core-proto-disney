d2be4e612e360be94ebbc15cee86c0a4e4b5bfb8
Quertte <a.injen90@gmail.com>
2022-08-15 18:13:54 +0300

    all done

--- src/person.js
+++ src/person.js
@@ -2,12 +2,29 @@
  * Человек.
  * @param {string} name
  */
-function Person(name) {
-  // Код тут.
+function Person(name, watchedMovies) {
+  this.name = name;
+  this.watchedMovies = watchedMovies;
+  this.watchedMovies.toString = function () {
+    return watchedMovies.reduce((a, el, ind) => a + (ind + 1) + " " + el.title, '');
+  }
 }
 
 Person.prototype.watchMovie = function (movie) {
-  // Код тут.
+  if (!this.watchedMovies.includes(movie)) {
+    this.watchedMovies.push(movie);
+  }
+  movie.play();
 };
 
+// Person.prototype.toString = function () {
+//   return this.watchedMovies.reduce((a, el, ind) => a + (ind + 1) + " " + el.title, '')
+// }
+
 module.exports = { Person };
+
+
+// const emma = new Person('Emma', ['rapunces', 'frozen']);
+// let prettyString = 'The movies which Emma has watched:\n' + emma.watchedMovies;
+// // console.log(emma.watchedMovies.toString());
+// console.log(prettyString);
